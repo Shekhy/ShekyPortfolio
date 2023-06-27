cd31687fedce699f9904400eefc0d31585553900
Shekhy <babin.marko@gmail.com>
2023-06-27 22:48:43 +0200

    Updated status for enum

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,9 +40,15 @@ class PortfoliosController < ApplicationController
     end
 
     def destroy
-      @blog.destroy
+      # Perform the lookup
+      @portfolio_item = Portfolio.find(params[:id])
+
+      # Destroy/delete the record
+      @portfolio_item.destroy
+
+      # Redirect
       respond_to do |format|
-        format.html { redirect_to blogs_url, notice: "Post was removed." }
+        format.html { redirect_to portfolios_url, notice: "Record was removed." }
       end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_213648) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_200713) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
+    t.integer "status", default: 0
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|

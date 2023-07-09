0337e8a1b96763d5c6a0f7002d7f67cd9c82e584
Shekhy <babin.marko@gmail.com>
2023-07-10 00:40:02 +0200

    Implemened concern for managing image data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_30_210210) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_214904) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_210210) do
     t.integer "precent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
